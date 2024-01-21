cff75192419505eb1fcef49a9d3d95d90e14d513
Diomon3 <gongyi002@gmail.com>
2024-01-21 11:24:24 +0800

    .

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-const canvas = 
+const canvas = document.querySelector("canvas")
+const con = canvas.getContext("2d")
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Player{
+    constructor(x, y, radius, color){
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
+    draw(){
+        con.beginPath()
+        con.arc(this.x, this.y, radius, 0, Math.PI*2, false)
+        con.fillStyle = this.color
+        con.fill()
+    }
+}
+const player = new Player(canvas.width/2, canvas.height/2, 30, "blue")
+player.draw()
