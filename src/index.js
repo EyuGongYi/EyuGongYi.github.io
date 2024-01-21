9061b6a7c25fb3f0ac61ccba433ef1e845c43224
Diomon3 <gongyi002@gmail.com>
2024-01-21 11:47:12 +0800

    .

--- src/index.js
+++ src/index.js
@@ -17,5 +17,24 @@ class Player{
         con.fill()
     }
 }
+class Projectile{
+    constructor(x,y,radius, color, velocity){
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+    draw(){
+        con.beginPath()
+        con.arc(this.x, this.y, this.radius, 0, Math.PI*2, false)
+        con.fillStyle = this.color
+        con.fill()
+    }
+}
+const
 const player = new Player(canvas.width/2, canvas.height/2, 30, "blue")
 player.draw()
+window.addEventListener("click",(event)=>{
+    const projectile = new Projectile(event.clientX, event.clientY, 30, "red", null)
+})
